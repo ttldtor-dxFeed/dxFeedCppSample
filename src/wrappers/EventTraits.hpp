7fed8c7f349810ddcaac3b9e5e0fc7f84dc28267
ttldtor <ttldtor@gmail.com>
2022-08-08 19:10:27 +0300

    Implement Subscription

--- src/wrappers/EventTraits.hpp
+++ src/wrappers/EventTraits.hpp
@@ -17,7 +17,11 @@ struct UnknownEventType {};
 
 struct EventTraitsBase {
     using Type = UnknownEventType;
-    static DXFCPP_USE_CONSTEXPR EventType eventType = EventType::Unknown;
+
+    static EventType getEventType() {
+        return EventType::UNKNOWN;
+    }
+
     static DXFCPP_USE_CONSTEXPR bool isSpecialized = false;
     using CApiEventType = UnknownCApiEventType;
     static DXFCPP_USE_CONSTEXPR unsigned cApiEventId = 0u;
@@ -50,7 +54,11 @@ struct Candle;
 template<>
 struct EventTraits<Quote> : public EventTraitsBase {
     using Type = Quote;
-    static DXFCPP_USE_CONSTEXPR EventType eventType = EventType::Quote;
+
+    static EventType getEventType() {
+        return EventType::QUOTE;
+    }
+
     static DXFCPP_USE_CONSTEXPR bool isSpecialized = true;
     using CApiEventType = dxf_quote_t;
     static DXFCPP_USE_CONSTEXPR unsigned cApiEventId = dx_eid_quote;
@@ -62,7 +70,11 @@ struct EventTraits<Quote> : public EventTraitsBase {
 template<>
 struct EventTraits<Candle> : public EventTraitsBase {
     using Type = Candle;
-    static DXFCPP_USE_CONSTEXPR EventType eventType = EventType::Candle;
+
+    static EventType getEventType() {
+        return EventType::CANDLE;
+    }
+
     static DXFCPP_USE_CONSTEXPR bool isSpecialized = true;
     using CApiEventType = dxf_candle_t;
     static DXFCPP_USE_CONSTEXPR unsigned cApiEventId = dx_eid_candle;
@@ -70,6 +82,21 @@ struct EventTraits<Candle> : public EventTraitsBase {
     static DXFCPP_USE_CONSTEXPR bool isIndexedEvent = true;
     static DXFCPP_USE_CONSTEXPR bool isLastingEvent = true;
     static DXFCPP_USE_CONSTEXPR bool isTimeSeriesEvent = true;
+};
+
+template<unsigned cApiEventMask>
+struct CApiEventMaskToEvent {
+    using Type = UnknownEventType;
+};
+
+template<>
+struct CApiEventMaskToEvent<DXF_ET_QUOTE> {
+    using Type = Quote;
+};
+
+template<>
+struct CApiEventMaskToEvent<DXF_ET_CANDLE> {
+    using Type = Candle;
 };
 
 } // namespace dxfcpp
--- src/wrappers/Subscription.hpp
+++ src/wrappers/Subscription.hpp
@@ -11,17 +11,85 @@
 
 #include "DXFCppConfig.hpp"
 
+#include "EventTraits.hpp"
 #include "EventType.hpp"
+
+#include <variant.hpp>
+
+#include "helpers/Handler.hpp"
+
+#include "Candle.hpp"
+#include "Quote.hpp"
 
 namespace dxfcpp {
 
 struct Connection;
 
-struct Subscription {
-    dxf_subscription_t subscriptionHandle_ = nullptr;
+struct SubscriptionBase {
+    using Event = nonstd::variant<Quote, Candle>;
+    using TimeSeriesEvent = nonstd::variant<Candle>;
+    using IndexedEvent = nonstd::variant<Candle>;
+};
 
-    static std::shared_ptr<Subscription> create(std::weak_ptr<Connection> connection,
-                                                const std::vector<EventType> &eventTypes);
+struct Subscription : public SubscriptionBase {
+    template <typename E> class Impl {
+        mutable std::recursive_mutex mutex_{};
+        dxf_subscription_t subscriptionHandle_ = nullptr;
+        std::function<void(int /* eventType */, dxf_const_string_t /* symbolName */,
+                           const dxf_event_data_t * /* eventData */, int /*dataCount (always 1) */,
+                           void * /* userData */)>
+            eventListener_{};
+
+        Handler<void(const Event &)> eventsHandler_{};
+
+        friend Subscription;
+
+      public:
+        ~Impl() {
+            std::lock_guard<std::recursive_mutex> lock{mutex_};
+
+            if (subscriptionHandle_ != nullptr) {
+                dxf_close_subscription(subscriptionHandle_);
+                subscriptionHandle_ = nullptr;
+            }
+        }
+    };
+
+    static std::shared_ptr<Impl<Event>> create(dxf_connection_t connectionHandle,
+                                               const EventTypesMask &eventTypesMask) {
+        auto s = std::make_shared<Impl<Event>>();
+        dxf_subscription_t subscriptionHandle = nullptr;
+
+        auto r =
+            dxf_create_subscription(connectionHandle, static_cast<int>(eventTypesMask.getMask()), &subscriptionHandle);
+
+        if (r == DXF_FAILURE) {
+            return {};
+        }
+
+        auto eventListener = [](int eventType, dxf_const_string_t symbolName, const dxf_event_data_t *eventData,
+                                int /*dataCount (always 1) */, void *userData) {
+            auto symbol = StringConverter::wStringToUtf8(symbolName);
+
+            switch (static_cast<unsigned>(eventType)) {
+            case DXF_ET_QUOTE:
+                reinterpret_cast<Impl<Event> *>(userData)->eventsHandler_(
+                    Quote(symbol, *reinterpret_cast<const dxf_quote_t *>(eventData)));
+            case DXF_ET_CANDLE:
+                reinterpret_cast<Impl<Event> *>(userData)->eventsHandler_(
+                    Candle(symbol, *reinterpret_cast<const dxf_candle_t *>(eventData)));
+            }
+
+            return;
+        };
+
+        dxf_attach_event_listener(subscriptionHandle, eventListener, reinterpret_cast<void *>(s.get()));
+
+        s->subscriptionHandle_ = subscriptionHandle;
+        s->eventListener_ = eventListener;
+
+        return s;
+    }
 
     void addSymbol(const std::string &symbol);
 
