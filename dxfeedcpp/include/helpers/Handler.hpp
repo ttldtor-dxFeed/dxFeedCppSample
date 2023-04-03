9a04afdc6123b0ad0c8eb04f4f684bc2637f610c
ttldtor <ttldtor@gmail.com>
2023-04-03 15:34:41 +0300

    Update Handler.hpp

--- dxfeedcpp/include/helpers/Handler.hpp
+++ dxfeedcpp/include/helpers/Handler.hpp
@@ -14,66 +14,87 @@
 #include <vector>
 
 namespace dxfcpp {
-
-template <typename Signature> struct Handler;
+/**
+ * A thread-safe class that allows to asynchronously notify listeners with a given signature.
+ * Listeners can be any callable entities.
+ * Listeners are placed in a future. Within one batch, listeners will be called sequentially. Futures are placed in
+ * a circular buffer and executed asynchronously.
+ *
+ * If you need synchronous execution of listeners, but it was possible to execute them in another thread, then limit
+ * the buffer size to one.
+ *
+ * @tparam Signature The arguments "signature" (example: `void(int, int)`)
+ */
+template<typename Signature>
+struct Handler;
 
 /**
  * A thread-safe class that allows to asynchronously notify listeners with a given signature.
  * Listeners can be any callable entities.
- * This class executes incoming events sequentially using a simple fixed-length circular buffer implementation.
+ * Listeners are placed in a future. Within one batch, listeners will be called sequentially. Futures are placed in
+ * a circular buffer and executed asynchronously.
  *
- * @tparam ArgTypes The arguments "signature"
+ * If you need synchronous execution of listeners, but it was possible to execute them in another thread, then limit
+ * the buffer size to one.
+ *
+ * @tparam ArgTypes The arguments "signature" (example: `void(int, int)`)
  */
-template <typename... ArgTypes> struct Handler<void(ArgTypes...)> final {
-    ///
+template<typename... ArgTypes>
+struct Handler<void(ArgTypes...)> final {
+    /// The listener type
     using ListenerType = std::function<void(ArgTypes...)>;
 
-  private:
-    static const unsigned MAIN_FUTURES_DEFAULT_SIZE = 1024;
+private:
+    static constexpr unsigned MAIN_FUTURES_DEFAULT_SIZE = 1024;
 
     std::recursive_mutex listenersMutex_{};
     std::unordered_map<std::size_t, ListenerType> listeners_{};
+    std::unordered_map<std::size_t, ListenerType> lowPriorityListeners_{};
     std::size_t lastId_{};
 
     std::recursive_mutex mainFuturesMutex_{};
     std::vector<std::shared_future<void>> mainFutures_{};
     std::size_t mainFuturesCurrentIndex_{};
-    std::size_t mainFuturesSize_{};
+    const std::size_t mainFuturesSize_{};
 
-    std::shared_future<void> handleImpl(ArgTypes &&...args) {
+    std::shared_future<void> handleImpl(ArgTypes...args) {
         return std::async(
-            std::launch::async,
-            [this](ArgTypes &&...args) {
-                std::lock_guard<std::recursive_mutex> guard{listenersMutex_};
+                std::launch::async,
+                [this](ArgTypes...args) {
+                    std::lock_guard guard{listenersMutex_};
 
-                for (auto &listener : listeners_) {
-                    listener.second(args...);
-                }
-            },
-            std::forward<ArgTypes>(args)...);
+                    for (auto &listener: listeners_) {
+                        listener.second(args...);
+                    }
+
+                    for (auto &listener: lowPriorityListeners_) {
+                        listener.second(args...);
+                    }
+                },
+                args...);
     }
 
-  public:
+public:
     /**
      * Creates the new handler by specified size of circular buffer of futures
      *
      * @param mainFuturesSize The size of the circular buffer of futures
      */
     explicit Handler(std::size_t mainFuturesSize = MAIN_FUTURES_DEFAULT_SIZE)
-        : mainFuturesCurrentIndex_{0ULL}, mainFuturesSize_{mainFuturesSize} {
+            : mainFuturesCurrentIndex_{0ULL}, mainFuturesSize_{mainFuturesSize} {
         mainFutures_.reserve(mainFuturesSize);
     }
 
     /**
-     * Calls the listeners and pass the ars to them
+     * Calls the listeners and pass the args to them
      *
      * @param args The listeners arguments
      */
-    void handle(ArgTypes &&...args) {
-        auto f = handleImpl(std::forward<ArgTypes>(args)...);
+    void handle(ArgTypes...args) {
+        auto f = handleImpl(args...);
 
         {
-            std::lock_guard<std::recursive_mutex> guard{mainFuturesMutex_};
+            std::lock_guard guard{mainFuturesMutex_};
 
             if (mainFutures_.size() < mainFuturesSize_) {
                 mainFutures_.emplace_back(f);
@@ -91,16 +112,16 @@ template <typename... ArgTypes> struct Handler<void(ArgTypes...)> final {
      *
      * @param args The listeners arguments
      */
-    void operator()(ArgTypes &&...args) { return handle(std::forward<ArgTypes>(args)...); }
+    void operator()(ArgTypes...args) { return handle(args...); }
 
     /**
-     * Adds the listener
+     * Adds the listener to "main" group
      *
      * @param listener The listener
      * @return The listener id
      */
     std::size_t add(ListenerType &&listener) {
-        std::lock_guard<std::recursive_mutex> guard{listenersMutex_};
+        std::lock_guard guard{listenersMutex_};
 
         lastId_++;
         listeners_.emplace(lastId_, std::forward<ListenerType>(listener));
@@ -109,7 +130,23 @@ template <typename... ArgTypes> struct Handler<void(ArgTypes...)> final {
     }
 
     /**
-     * Adds the listener
+     * Adds the low priority listener (to the "low priority" group)
+     * It will be called after the "main" listeners
+     *
+     * @param listener The listener
+     * @return The listener id
+     */
+    std::size_t addLowPriority(ListenerType &&listener) {
+        std::lock_guard guard{listenersMutex_};
+
+        lastId_++;
+        lowPriorityListeners_.emplace(lastId_, std::forward<ListenerType>(listener));
+
+        return lastId_;
+    }
+
+    /**
+     * Adds the listener to "main" group
      *
      * @param listener The listener
      * @return The listener id
@@ -117,15 +154,26 @@ template <typename... ArgTypes> struct Handler<void(ArgTypes...)> final {
     std::size_t operator+=(ListenerType &&listener) { return add(std::forward<ListenerType>(listener)); }
 
     /**
+     * Adds the low priority listener (to the "low priority" group).
+     * It will be called after the "main" listeners
+     *
+     * @param listener The listener
+     * @return The listener id
+     */
+    std::size_t operator%=(ListenerType &&listener) { return addLowPriority(std::forward<ListenerType>(listener)); }
+
+    /**
      * Removes a listener by the id
      *
      * @param id The listener id
      */
     void remove(std::size_t id) {
-        std::lock_guard<std::recursive_mutex> guard{listenersMutex_};
+        std::lock_guard guard{listenersMutex_};
 
         if (listeners_.count(id) > 0) {
             listeners_.erase(id);
+        } else if (lowPriorityListeners_.count(id) > 0) {
+            lowPriorityListeners_.erase(id);
         }
     }
 
